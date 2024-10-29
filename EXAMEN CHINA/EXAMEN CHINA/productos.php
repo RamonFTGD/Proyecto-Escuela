e5e7131af5e33b8f33ca75bb5d8ee2aec2a09784
Ramón <165967820+RamonFTGD@users.noreply.github.com>
2024-10-29 13:11:26 -0600

    Update productos.php

--- EXAMEN CHINA/EXAMEN CHINA/productos.php
+++ EXAMEN CHINA/EXAMEN CHINA/productos.php
@@ -1,17 +1,5 @@
 <?php
-    $host = 'localhost';
-    $db = 'papeleria';
-    $user = 'root';
-    $password = '';
-    try{
-        $conn = new PDO("mysql:host=$host;dbname=$db", $user, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        die("Error de conexion: " . $e->getMessage());
-    }
-    $clientExist = function($id){
-        return $id;
-    }
+    include "functions.php";
 ?>
 
 <!DOCTYPE html>
