c29425a6ff3a02f82b60b5579af13c8097fe0034
Daniel Ramirez <148291696+Danierf1717@users.noreply.github.com>
2024-10-29 19:18:02 -0600

    Update productos.php

--- EXAMEN CHINA/EXAMEN CHINA/productos.php
+++ EXAMEN CHINA/EXAMEN CHINA/productos.php
@@ -7,190 +7,168 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Papelería</title>
 </head>
 <body>
-<header>
+    <header>
         <div class="logo">
-            <a href="papeleria.php"><img src="imagenes/Horizon Mobile Devices.png" alt="Horizon Mobile Devices"></a>
+            <a href="papeleria.php"><img src="imagenes/Horizon Mobile Devices.png" alt="Papelería"></a>
         </div>
         <nav>
             <ul>
-                <li><a href="papeleria.php">Papeleria</a></li>
-                <li><a href="productos.php">Catalago de Productos</a></li>
+                <li><a href="papeleria.php">Papelería</a></li>
+                <li><a href="productos.php">Catálogo de Productos</a></li>
                 <li><a href="vender.php">Vender</a></li>
                 <li><a href="index.php">Volver al Inicio</a></li>
             </ul>
         </nav>
     </header>
     <main>
-        <section class="horizon-mobile">
-            <div class="horizon-mobile-inner">
-                <h2></h2>
+        <section class="papeleria">
+            <div class="papeleria-inner">
+                <h2>Artículos de Papelería</h2>
                 <div class="video-container">
-                    <video src="imagenes/12 Accesorios ESENCIALES para tu Hprizon Mobile!!.mp4" autoplay muted loop playsinline></video>
+                    <video src="imagenes/AccesoriosPapeleria.mp4" autoplay muted loop playsinline></video>
                 </div>
-                <p>Los accesorios de Horizon Mobile Devices están diseñados para complementar y mejorar tu experiencia con tus dispositivos favoritos.</p>
+                <p>Los productos de nuestra tienda de papelería están diseñados para facilitar tus tareas escolares, de oficina y creativas.</p>
                 <ul>
-                    <li><strong>Fundas y Estuches:</strong> Protege tus dispositivos con una variedad de fundas y estuches elegantes y duraderos.</li>
-                    <li><strong>Protectores de Pantalla:</strong> Mantén tu pantalla libre de arañazos y daños con nuestros protectores de pantalla de alta calidad.</li>
-                    <li><strong>Cargadores:</strong> Desde cargadores rápidos hasta inalámbricos, mantén tus dispositivos siempre cargados y listos para usar.</li>
-                    <li><strong>Cables:</strong> Conectividad garantizada con una amplia gama de cables de alta durabilidad y rendimiento.</li>
-                    <li><strong>Teclados:</strong> Aumenta tu productividad con teclados inalámbricos y ergonómicos.</li>
-                    <li><strong>Ratones:</strong> Ratones precisos y cómodos para mejorar tu experiencia de navegación y trabajo.</li>
-                    <li><strong>Adaptadores:</strong> Soluciones de conectividad para todas tus necesidades de dispositivos y periféricos.</li>
-                    <li><strong>AirTags:</strong> Localiza tus objetos personales con nuestros rastreadores compactos y eficientes.</li>
-                    <li><strong>Correas para Horizon Watch:</strong> Personaliza tu Apple Watch con una variedad de correas de diferentes estilos y materiales.</li>
-                    <li><strong>Altavoces y Auriculares:</strong> Disfruta de un sonido superior con nuestra gama de altavoces y auriculares de alta calidad.</li>
+                    <li><strong>Lápices:</strong> Variedad de lápices para dibujo y escritura.</li>
+                    <li><strong>Lapiceras:</strong> Lapiceras de diferentes colores y estilos.</li>
+                    <li><strong>Colores:</strong> Amplia gama de colores para tus proyectos creativos.</li>
+                    <li><strong>Sacapuntas:</strong> Sacapuntas de alta calidad para mantener tus lápices en óptimas condiciones.</li>
+                    <li><strong>Borradores:</strong> Borradores efectivos y de larga duración.</li>
+                    <li><strong>Pegamento:</strong> Pegamento en barra, líquido y en diferentes tamaños.</li>
+                    <li><strong>Tijeras:</strong> Tijeras para cortes precisos y cómodos.</li>
+                    <li><strong>Cartucheras:</strong> Diferentes modelos de cartucheras para organizar tu papelería.</li>
+                    <li><strong>Memoria USB:</strong> Almacenamiento portátil para tus documentos y archivos.</li>
                 </ul>
                 <a href="#" class="learn-more-btn" onclick="openModal('modalGeneral')">Más información</a>
             </div>
-            <img src="img/accesorios_image.png" alt="Accesorios" class="device-img">
+            <img src="img/papeleria_image.png" alt="Papelería" class="device-img">
         </section>
         
         <section id="productos">
             <div class="producto" id="producto1">
-                <h2>Fundas y Estuches</h2>
-                <img src="imagenes/funda.webp" alt="Fundas y Estuches">
-                <p>Precio: $70</p>
-                <a href="#" class="learn-more-btn" onclick="openModal('modalFundas')">Más información</a>
+                <h2>Lápices</h2>
+                <img src="imagenes/lapiz.jpg" alt="Lápices">
+                <p>Precio: $10</p>
+                <a href="#" class="learn-more-btn" onclick="openModal('modalLapices')">Más información</a>
             </div>
             <div class="producto" id="producto2">
-                <h2>Protectores de Pantalla</h2>
-                <img src="imagenes/protectorpantalla.jpg" alt="Protectores de Pantalla">
-                <p>Precio: $25</p>
-                <a href="#" class="learn-more-btn" onclick="openModal('modalProtectores')">Más información</a>
+                <h2>Lapiceras</h2>
+                <img src="imagenes/lapicera.jpg" alt="Lapiceras">
+                <p>Precio: $15</p>
+                <a href="#" class="learn-more-btn" onclick="openModal('modalLapiceras')">Más información</a>
             </div>
             <div class="producto" id="producto3">
-                <h2>Cargadores</h2>
-                <img src="imagenes/cargador.webp" alt="Cargadores">
-                <p>Precio: $300</p>
-                <a href="#" class="learn-more-btn" onclick="openModal('modalCargadores')">Más información</a>
+                <h2>Colores</h2>
+                <img src="imagenes/colores.jpg" alt="Colores">
+                <p>Precio: $50</p>
+                <a href="#" class="learn-more-btn" onclick="openModal('modalColores')">Más información</a>
             </div>
             <div class="producto" id="producto4">
-                <h2>Cables</h2>
-                <img src="imagenes/cable.jpg" alt="Cables">
-                <p>Precio: $30</p>
-                <a href="#" class="learn-more-btn" onclick="openModal('modalCables')">Más información</a>
+                <h2>Sacapuntas</h2>
+                <img src="imagenes/sacapuntas.jpg" alt="Sacapuntas">
+                <p>Precio: $5</p>
+                <a href="#" class="learn-more-btn" onclick="openModal('modalSacapuntas')">Más información</a>
             </div>
             <div class="producto" id="producto5">
-                <h2>Teclados</h2>
-                <img src="imagenes/teclado.png" alt="Teclados">
-                <p>Precio: $100</p>
-                <a href="#" class="learn-more-btn" onclick="openModal('modalTeclados')">Más información</a>
+                <h2>Borradores</h2>
+                <img src="imagenes/borrador.jpg" alt="Borradores">
+                <p>Precio: $8</p>
+                <a href="#" class="learn-more-btn" onclick="openModal('modalBorradores')">Más información</a>
             </div>
             <div class="producto" id="producto6">
-                <h2>Ratones</h2>
-                <img src="imagenes/raton.jpg" alt="Ratones">
-                <p>Precio: $500</p>
-                <a href="#" class="learn-more-btn" onclick="openModal('modalRatones')">Más información</a>
+                <h2>Pegamento</h2>
+                <img src="imagenes/pegamento.jpg" alt="Pegamento">
+                <p>Precio: $20</p>
+                <a href="#" class="learn-more-btn" onclick="openModal('modalPegamento')">Más información</a>
             </div>
             <div class="producto" id="producto7">
-                <h2>Adaptadores</h2>
-                <img src="imagenes/adaptadorjh.webp" alt="Adaptadores">
-                <p>Precio: $50</p>
-                <a href="#" class="learn-more-btn" onclick="openModal('modalAdaptadores')">Más información</a>
+                <h2>Tijeras</h2>
+                <img src="imagenes/tijeras.jpg" alt="Tijeras">
+                <p>Precio: $30</p>
+                <a href="#" class="learn-more-btn" onclick="openModal('modalTijeras')">Más información</a>
             </div>
             <div class="producto" id="producto8">
-                <h2>AirTags</h2>
-                <img src="imagenes/airtag.jpg" alt="AirTags">
-                <p>Precio: $70</p>
-                <a href="#" class="learn-more-btn" onclick="openModal('modalAirTags')">Más información</a>
+                <h2>Cartucheras</h2>
+                <img src="imagenes/cartuchera.jpg" alt="Cartucheras">
+                <p>Precio: $25</p>
+                <a href="#" class="learn-more-btn" onclick="openModal('modalCartucheras')">Más información</a>
             </div>
             <div class="producto" id="producto9">
-                <h2>Correas para Horizon Watch</h2>
-                <img src="imagenes/correa2.webp" alt="Correas para Horizon Watch">
-                <p>Precio: $50</p>
-                <a href="#" class="learn-more-btn" onclick="openModal('modalCorreas')">Más información</a>
-            </div>
-            <div class="producto" id="producto10">
-                <h2>Altavoces y Auriculares</h2>
-                <img src="imagenes/homepod__eam53jjm772a_large.jpg" alt="Altavoces y Auriculares">
-                <p>Precio: $499</p>
-                <a href="#" class="learn-more-btn" onclick="openModal('modalAltavoces')">Más información</a>
+                <h2>Memoria USB</h2>
+                <img src="imagenes/memoriausb.jpg" alt="Memoria USB">
+                <p>Precio: $100</p>
+                <a href="#" class="learn-more-btn" onclick="openModal('modalUSB')">Más información</a>
             </div>
         </section>
 
-        <!-- Modal General -->
-        <div id="modalGeneral" class="modal">
-            <div class="modal-content">
-                <span class="close" onclick="closeModal('modalGeneral')">&times;</span>
-                <h2>Accesorios</h2>
-                <p>Los accesorios de Horizon Mobile Devices incluyen una variedad de productos diseñados para mejorar tu experiencia con tus dispositivos favoritos. Ya sea que necesites proteger tu dispositivo, aumentar su funcionalidad o simplemente personalizarlo, tenemos lo que necesitas.</p>
-            </div>
-        </div>
-
         <!-- Modales Específicos -->
-        <div id="modalFundas" class="modal">
+        <div id="modalLapices" class="modal">
             <div class="modal-content">
-                <span class="close" onclick="closeModal('modalFundas')">&times;</span>
-                <h2>Fundas y Estuches</h2>
-                <p>Nuestras fundas y estuches están diseñados para ofrecer la máxima protección y estilo a tus dispositivos. Hechos con materiales de alta calidad, garantizan durabilidad y resistencia a impactos.</p>
+                <span class="close" onclick="closeModal('modalLapices')">&times;</span>
+                <h2>Lápices</h2>
+                <p>Una selección de lápices de alta calidad, perfectos para escribir y dibujar.</p>
             </div>
         </div>
-        <div id="modalProtectores" class="modal">
+        <div id="modalLapiceras" class="modal">
             <div class="modal-content">
-                <span class="close" onclick="closeModal('modalProtectores')">&times;</span>
-                <h2>Protectores de Pantalla</h2>
-                <p>Mantén tu pantalla en perfectas condiciones con nuestros protectores de pantalla de alta calidad. Protegen contra arañazos, polvo y caídas, asegurando una visibilidad clara y sin interrupciones.</p>
+                <span class="close" onclick="closeModal('modalLapiceras')">&times;</span>
+                <h2>Lapiceras</h2>
+                <p>Lapiceras de tinta fluida, disponibles en varios colores y diseños.</p>
             </div>
         </div>
-        <div id="modalCargadores" class="modal">
+         <div id="modalColores" class="modal">
             <div class="modal-content">
-                <span class="close" onclick="closeModal('modalCargadores')">&times;</span>
-                <h2>Cargadores</h2>
-                <p>Nuestros cargadores están diseñados para ofrecer una carga rápida y eficiente. Disponibles en varias configuraciones, incluyendo cargadores inalámbricos y de carga rápida, siempre tendrás energía cuando la necesites.</p>
+                <span class="close" onclick="closeModal('modalLapiceras')">&times;</span>
+                <h2>Colores</h2>
+                <p>Colores de la mas alta calidad, disponibles en varios colores y diseños.</p>
             </div>
         </div>
-        <div id="modalCables" class="modal">
+          <div id="modalSacapuntas" class="modal">
             <div class="modal-content">
-                <span class="close" onclick="closeModal('modalCables')">&times;</span>
-                <h2>Cables</h2>
-                <p>Nuestros cables de alta durabilidad garantizan una conectividad estable y rápida. Disponibles en diferentes longitudes y tipos, siempre tendrás el cable adecuado para cualquier situación.</p>
+                <span class="close" onclick="closeModal('modalLapiceras')">&times;</span>
+                <h2>Sacapuntas</h2>
+                <p>Sacapuntas de la mas alta calidad, disponibles en varios colores y diseños.</p>
             </div>
         </div>
-        <div id="modalTeclados" class="modal">
+          <div id="modalBorradores" class="modal">
             <div class="modal-content">
-                <span class="close" onclick="closeModal('modalTeclados')">&times;</span>
-                <h2>Teclados</h2>
-                <p>Los teclados de Horizon Mobile Devices están diseñados para proporcionar comodidad y eficiencia. Con opciones inalámbricas y ergonómicas, mejorarás tu productividad y experiencia de uso.</p>
+                <span class="close" onclick="closeModal('modalLapiceras')">&times;</span>
+                <h2>Borradores</h2>
+                <p>Borradores de la mas alta calidad, disponibles en varios colores y diseños.</p>
             </div>
         </div>
-        <div id="modalRatones" class="modal">
+          <div id="modalCartuchera" class="modal">
             <div class="modal-content">
-                <span class="close" onclick="closeModal('modalRatones')">&times;</span>
-                <h2>Ratones</h2>
-                <p>Nuestros ratones ofrecen precisión y comodidad. Diseñados para uso prolongado, son ideales para el trabajo y el entretenimiento, garantizando una experiencia de usuario óptima.</p>
+                <span class="close" onclick="closeModal('modalLapiceras')">&times;</span>
+                <h2>Cartuchera</h2>
+                <p>Cartuchera de la mas alta calidad, disponibles en varios colores y diseños.</p>
             </div>
         </div>
-        <div id="modalAdaptadores" class="modal">
+          <div id="modalPegamento" class="modal">
             <div class="modal-content">
-                <span class="close" onclick="closeModal('modalAdaptadores')">&times;</span>
-                <h2>Adaptadores</h2>
-                <p>Los adaptadores de Horizon Mobile Devices proporcionan soluciones de conectividad para todas tus necesidades. Compatibles con una amplia gama de dispositivos, aseguran una conexión rápida y estable.</p>
+                <span class="close" onclick="closeModal('modalLapiceras')">&times;</span>
+                <h2>Pegamento</h2>
+                <p>Pegamento de la mas alta calidad, disponibles en varios colores y diseños.</p>
             </div>
         </div>
-        <div id="modalAirTags" class="modal">
+          <div id="modalUsb" class="modal">
             <div class="modal-content">
-                <span class="close" onclick="closeModal('modalAirTags')">&times;</span>
-                <h2>AirTags</h2>
-                <p>Localiza tus objetos personales con nuestros AirTags compactos y eficientes. Con una conectividad fiable y una precisión excepcional, nunca más perderás tus pertenencias.</p>
+                <span class="close" onclick="closeModal('modalLapiceras')">&times;</span>
+                <h2>Memoria Usb</h2>
+                <p>Las mejores USB de la mas alta calidad, disponibles en varios colores y diseños.</p>
             </div>
         </div>
-        <div id="modalCorreas" class="modal">
+          <div id="modalTijeras" class="modal">
             <div class="modal-content">
-                <span class="close" onclick="closeModal('modalCorreas')">&times;</span>
-                <h2>Correas para Horizon Watch</h2>
-                <p>Personaliza tu Horizon Watch con nuestras correas de diferentes estilos y materiales. Desde opciones deportivas hasta elegantes, siempre tendrás una correa adecuada para cada ocasión.</p>
+                <span class="close" onclick="closeModal('modalLapiceras')">&times;</span>
+                <h2>Tijeras</h2>
+                <p>Tijeras de la mas alta calidad, disponibles en varios colores y diseños.</p>
             </div>
         </div>
-        <div id="modalAltavoces" class="modal">
-            <div class="modal-content">
-                <span class="close" onclick="closeModal('modalAltavoces')">&times;</span>
-                <h2>Altavoces y Auriculares</h2>
-                <p>Disfruta de un sonido superior con nuestra gama de altavoces y auriculares de alta calidad. Diseñados para ofrecer una experiencia de audio envolvente, son perfectos para el entretenimiento y el trabajo.</p>
-            </div>
-        </div>
+      
     </main>
     <footer>
         <p>&copy; 2024 Daniel y Ramon. Todos los derechos reservados.</p>
@@ -215,3 +193,4 @@
     </script>
 </body>
 </html>
+
